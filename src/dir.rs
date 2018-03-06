f8b7b6c37fab6ed05255903162da74866a62f3a5
Dana Keeler <dkeeler@mozilla.com>
2018-03-05 16:06:39 -0800

    fetch microdescriptors on-demand (NB SEE BELOW)
    Obviously you wouldn't want to do this in a production implementation
    because this basically announces to anyone watching on the wire (in
    plain text, no less), "HEY I'M GONNA BUILD A CIRCUIT INVOLVING THESE
    NODES!" but this is just for demonstration purposes and so we don't have
    to download the entire universe.

--- src/dir.rs
+++ src/dir.rs
@@ -35,17 +35,19 @@ pub fn get_tor_peers(hostport: &str) -> TorPeerList {
         "http://{}/tor/status-vote/current/consensus-microdesc/",
         hostport
     );
-    TorPeerList::new(TorPeer::parse_all(hostport, do_get(&uri)))
+    TorPeerList::new(hostport, PreTorPeer::parse_all(do_get(&uri)))
 }
 
 #[derive(Debug)]
 pub struct TorPeerList {
-    peers: HashSet<TorPeer>,
+    hostport: String,
+    peers: HashSet<PreTorPeer>,
 }
 
 impl TorPeerList {
-    fn new(peer_list: Vec<TorPeer>) -> TorPeerList {
+    fn new(hostport: &str, peer_list: Vec<PreTorPeer>) -> TorPeerList {
         TorPeerList {
+            hostport: hostport.to_owned(),
             peers: HashSet::from_iter(peer_list),
         }
     }
@@ -58,7 +60,11 @@ impl TorPeerList {
             Some(node) => node.clone(),
             None => return None,
         };
-        self.peers.take(&node)
+        if let Some(taken) = self.peers.take(&node) {
+            Some(taken.to_tor_peer(&self.hostport))
+        } else {
+            None // this shouldn't happen but still
+        }
     }
 
     pub fn get_interior_node(&mut self) -> Option<TorPeer> {
@@ -66,7 +72,11 @@ impl TorPeerList {
             Some(node) => node.clone(),
             None => return None,
         };
-        self.peers.take(&node)
+        if let Some(taken) = self.peers.take(&node) {
+            Some(taken.to_tor_peer(&self.hostport))
+        } else {
+            None // this shouldn't happen but still
+        }
     }
 
     pub fn get_exit_node(&mut self) -> Option<TorPeer> {
@@ -77,22 +87,21 @@ impl TorPeerList {
             Some(node) => node.clone(),
             None => return None,
         };
-        self.peers.take(&node)
+        if let Some(taken) = self.peers.take(&node) {
+            Some(taken.to_tor_peer(&self.hostport))
+        } else {
+            None // this shouldn't happen but still
+        }
     }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
-pub struct TorPeer {
+struct PreTorPeer {
+    /// The microdescriptor hash string as parsed out of the "m somebase64..." line.
+    mdesc_hash: String,
     ip_address: Ipv4Addr,
-    port: u16,
-    /// I don't know what this is for.
-    rsa_public_key: Vec<u8>,
-    /// Ntor handshake key, right?
-    ntor_onion_key: [u8; 32],
-    /// sha-1 hash of the peer's RSA ID key (not the above rsa_public_key)
     node_id: [u8; 20],
-    /// Ed25519 identity public key
-    ed25519_id_key: [u8; 32],
+    port: u16,
     /// Is this an exit node?
     is_exit: bool,
     /// Is this a guard node?
@@ -101,9 +110,9 @@ pub struct TorPeer {
     is_usable: bool,
 }
 
-impl TorPeer {
-    fn parse_all(hostport: &str, response_string: String) -> Vec<TorPeer> {
-        let mut microdescs: Vec<TorPeer> = Vec::new();
+impl PreTorPeer {
+    fn parse_all(response_string: String) -> Vec<PreTorPeer> {
+        let mut microdescs = Vec::new();
         let mut router_line: Option<&str> = None;
         let mut mdesc_line: Option<&str> = None;
         let mut flags_line: Option<&str> = None;
@@ -120,8 +129,7 @@ impl TorPeer {
                 flags_line = Some(line);
             }
             if router_line.is_some() && mdesc_line.is_some() && flags_line.is_some() {
-                microdescs.push(TorPeer::new(
-                    hostport,
+                microdescs.push(PreTorPeer::new(
                     router_line.take().unwrap(),
                     mdesc_line.take().unwrap(),
                     flags_line.take().unwrap(),
@@ -131,12 +139,25 @@ impl TorPeer {
         microdescs
     }
 
-    fn new(hostport: &str, router_line: &str, m_hash_line: &str, flags_line: &str) -> TorPeer {
-        let keys_uri = format!(
-            "http://{}/tor/micro/d/{}",
-            hostport,
-            m_hash_line.split(" ").nth(1).unwrap()
-        );
+    fn new(router_line: &str, m_hash_line: &str, flags_line: &str) -> PreTorPeer {
+        let mut flags = flags_line.split(" ");
+        let router_parts: Vec<&str> = router_line.split(" ").collect();
+        let node_id: [u8; 20] =
+            util::slice_to_20_byte_array(&base64::decode(router_parts[2]).unwrap());
+        PreTorPeer {
+            mdesc_hash: m_hash_line.split(" ").nth(1).unwrap().to_owned(),
+            ip_address: router_parts[5].parse().unwrap(),
+            port: u16::from_str(router_parts[6]).unwrap(),
+            node_id: node_id,
+            is_exit: flags.find(|s| s == &"Exit").is_some(),
+            is_guard: flags.find(|s| s == &"Guard").is_some(),
+            is_usable: flags.find(|s| s == &"Running").is_some()
+                && flags.find(|s| s == &"Valid").is_some(),
+        }
+    }
+
+    fn to_tor_peer(&self, hostport: &str) -> TorPeer {
+        let keys_uri = format!("http://{}/tor/micro/d/{}", hostport, self.mdesc_hash);
         let keys_data = do_get(&keys_uri);
         let mut ntor_onion_key: [u8; 32] = [0; 32];
         let mut ed25519_id_key: [u8; 32] = [0; 32];
@@ -163,24 +184,32 @@ impl TorPeer {
                 ).unwrap());
             }
         }
-        let mut flags = flags_line.split(" ");
-        let router_parts: Vec<&str> = router_line.split(" ").collect();
-        let node_id: [u8; 20] =
-            util::slice_to_20_byte_array(&base64::decode(router_parts[2]).unwrap());
         TorPeer {
-            ip_address: router_parts[5].parse().unwrap(),
-            port: u16::from_str(router_parts[6]).unwrap(),
+            ip_address: self.ip_address,
+            port: self.port,
             rsa_public_key: base64::decode(&rsa_public_key_base64).unwrap(),
             ntor_onion_key: ntor_onion_key,
-            node_id: node_id,
             ed25519_id_key: ed25519_id_key,
-            is_exit: flags.find(|s| s == &"Exit").is_some(),
-            is_guard: flags.find(|s| s == &"Guard").is_some(),
-            is_usable: flags.find(|s| s == &"Running").is_some()
-                && flags.find(|s| s == &"Valid").is_some(),
+            node_id: self.node_id,
         }
     }
+}
 
+#[derive(Debug)]
+pub struct TorPeer {
+    ip_address: Ipv4Addr,
+    port: u16,
+    /// I don't know what this is for.
+    rsa_public_key: Vec<u8>,
+    /// Ntor handshake key, right?
+    ntor_onion_key: [u8; 32],
+    /// sha-1 hash of the peer's RSA ID key (not the above rsa_public_key)
+    node_id: [u8; 20],
+    /// Ed25519 identity public key
+    ed25519_id_key: [u8; 32],
+}
+
+impl TorPeer {
     /// Get the sha-1 hash of the node's RSA identity key. For use in the Ntor handshake.
     pub fn get_node_id(&self) -> [u8; 20] {
         self.node_id
